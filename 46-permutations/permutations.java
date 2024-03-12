d20c843842d7939a706183f8d94cb85420fc095e
Aniket <37346075+aniketm07@users.noreply.github.com>
2024-03-12 13:04:59 -0300

    Time: 1 ms (95.52%) | Memory: 44.3 MB (65.55%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,20 +3,22 @@ class Solution {
         List<List<Integer>> finalAns = new ArrayList<>();
         boolean[] pickNotPick = new boolean[nums.length];
         recursiveCall(nums, pickNotPick, finalAns, new ArrayList<>());
+
         return finalAns;
     }
 
     public void recursiveCall(int[] nums, boolean[] pickNotPick, List<List<Integer>> finalAns, List<Integer> list){
         if(list.size() == nums.length){
-            finalAns.add(list);
+            finalAns.add(new ArrayList<>(list));
             return;
         }
         for(int i=0; i<pickNotPick.length; i++){
             if(!pickNotPick[i]){
                 pickNotPick[i] = true;
-                List<Integer> newList = new ArrayList<>(list);
-                newList.add(nums[i]);
-                recursiveCall(nums, pickNotPick, finalAns, newList);
+                list.add(nums[i]);
+                recursiveCall(nums, pickNotPick, finalAns, list);
+                list.remove(list.size() - 1);
+                // make sure to mark the element as false for the other tree call.
                 pickNotPick[i] = false;
             }
         }
