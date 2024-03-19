63f8718060669e998be86a32400de857e1f584ad
Aniket <37346075+aniketm07@users.noreply.github.com>
2024-03-19 11:34:34 -0300

    Time: 3 ms (62.67%) | Memory: 44.4 MB (92.13%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,80 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        boolean[][] board = new boolean[n][n];
+        nQueens(board, ans, 0, n);
+        return ans;
+    }
+
+    public void nQueens(boolean[][] board, List<List<String>> ans, int col, int n){
+        if(col == n){
+            List<String> list = new ArrayList<>();
+            for(int row=0;row<n;row++){
+                StringBuilder sb = new StringBuilder();
+                for(int column=0; column<n; column++){
+                    if(board[row][column]){
+                        sb.append("Q");
+                    }else{
+                        sb.append(".");
+                    }
+                }
+                list.add(sb.toString());
+            }
+            ans.add(list);
+            return;
+        }
+
+        for(int row=0;row<n;row++){
+            if(!checkClash(row, col, board, n)){
+                board[row][col] = true;
+                nQueens(board, ans, col+1, n);
+                board[row][col] = false;
+            }
+        }
+    }
+
+    public Boolean checkClash(int row, int col, boolean[][] board, int n){
+        for(int i=0;i<n;i++){
+            if(board[row][i] || board[i][col]){
+                return true;
+            }
+        }
+        int i=row-1;
+        int j=col-1;
+        while(checkBounds(i, j, n)){
+            if(board[i][j]){
+                return true;
+            }
+            i--;j--;
+        }
+        i=row+1;
+        j=col+1;
+        while(checkBounds(i, j, n)){
+            if(board[i][j]){
+                return true;
+            }
+            i++;j++;
+        }
+        i=row+1;
+        j=col-1;
+        while(checkBounds(i, j, n)){
+            if(board[i][j]){
+                return true;
+            }
+            i++;j--;
+        }
+        i=row-1;
+        j=col+1;
+        while(checkBounds(i, j, n)){
+            if(board[i][j]){
+                return true;
+            }
+            i--;j++;
+        }
+        return false;
+    }
+
+    public boolean checkBounds(int i, int j, int n){
+        return i>=0 && j>=0 && i<n && j<n;
+    }
+}
