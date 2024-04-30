9ed65fc966773036f00f679f16a7af2577cb691c
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2024-04-29 23:06:41 -0300

    Time: 11 ms (33.99%) | Memory: 41.2 MB (33.19%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        recursiveCall(board);
+    }
+
+    public boolean recursiveCall(char[][] board){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.'){
+                    for(int val=1; val<=9; val++){
+                        if(isValid(board, i, j, (char)(val + '0'))){
+                            board[i][j] = (char) (val + '0');
+                            if(recursiveCall(board)){
+                                return true;
+                            }else{
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean isValid(char[][] board, int row, int col, char c) {
+    for (int i = 0; i < 9; i++) {
+      if (board[i][col] == c)
+        return false;
+
+      if (board[row][i] == c)
+        return false;
+
+      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+        return false;
+    }
+    return true;
+  }
+}
