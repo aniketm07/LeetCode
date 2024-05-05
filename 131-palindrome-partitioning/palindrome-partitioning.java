ad7a334424868685ec232f0098798a8b022dee04
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2024-05-05 16:01:12 -0300

    Time: 12 ms (16.64%) | Memory: 55.9 MB (93.70%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    
+    public List<List<String>> partition(String s) {
+        List<List<String>> finalAns = new ArrayList<>();
+        List<String> currentList = new ArrayList<>();
+        recursionCall(s, 0, currentList, finalAns);
+        return finalAns;
+    }
+
+    public static void recursionCall(String s, int index, List<String> currentList, List<List<String>> finalAns){
+        if(index >= s.length()){
+            finalAns.add(new ArrayList<>(currentList));
+            return;
+        }
+        for(int i=index+1; i<=s.length(); i++){
+            if(isPalindrome(s.substring(index, i))){
+                currentList.add(s.substring(index, i));
+                recursionCall(s, i, currentList, finalAns);
+                currentList.remove(currentList.size()-1);
+            }
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
