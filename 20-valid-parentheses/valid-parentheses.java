b2155d7c28c9694f0d8e7da1f09efd2e2880a9c4
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2024-10-10 01:04:57 -0300

    Time: 41 ms (5.22%) | Memory: 45.6 MB (5.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,27 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2!=0){
+        while(s.contains("{}") || s.contains("()") || s.contains("[]")){
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
+        }
+        if(s.length()!=0){
             return false;
         }
-        Stack<Character> stack = new Stack();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
-                stack.push(s.charAt(i));   
-            }else{
-                if(stack.isEmpty() || s.charAt(i)!=getChar(stack.pop())){
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
-    
-    public Character getChar(Character c){
-        switch(c){
-            case '{': return '}';
-            case '(': return ')';
-            case '[': return ']';
-            default : return null;
-        }
+        return true;
     }
 }
