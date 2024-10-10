5a76421d5ccb77076734b0f134a4e3a48c47e641
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2024-10-10 00:51:56 -0300

    Time: 2 ms (52.91%) | Memory: 41.7 MB (24.74%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        List<Character> list = Arrays.asList('{', '[', '(');
+        Map<Character,Character> map = new HashMap<>() {{
+            put('}', '{');
+            put(')', '(');
+            put(']', '[');
+        }};
+        
+        for(char ch : s.toCharArray()){
+            if(list.contains(ch)){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                Character pop = stack.pop();
+                if(map.get(ch)!=pop){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
