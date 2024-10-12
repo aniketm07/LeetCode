3a933d9188e058d894d02a441a124a341675625d
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2024-10-12 01:55:42 -0300

    Time: 271 ms (22.81%) | Memory: 45.1 MB (14.01%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,7 +1,11 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         int val = (int)(b.length() / a.length());
-        String tryString = String.join("", Collections.nCopies(val, a));
+        String tryString = "";
+        for(int i=0; i<val; i++){
+            tryString+=a;
+        }
+    
         for(int i=val;i<val+3;i++){
             if(tryString.contains(b)){
                 return i;
