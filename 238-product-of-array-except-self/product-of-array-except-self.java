7ef86719b7c9ea31a03aecd28a801171294ee8af
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-01 19:25:20 -0300

    Time: 2 ms (87.93%) | Memory: 55.3 MB (75.24%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,39 +1,15 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] prefix = new int[nums.length];
-        int[] postfix = new int[nums.length];
         int[] output = new int[nums.length];
-
-        for(int i=0;i<n;i++){
-            if(i==0){
-                prefix[i] = nums[i];
-            }else{
-                prefix[i] = nums[i]*prefix[i-1];
-            }
+        Arrays.fill(output, 1);
+        for(int i=1;i<n;i++){
+            output[i] = nums[i-1]*output[i-1];
         }
+        int postFix = 1;
         for(int i=n-1;i>=0;i--){
-            if(i==n-1){
-                postfix[i] = nums[i];
-            }else{
-                postfix[i] = nums[i]*postfix[i+1];
-            }
-        }
-        // for(int i=0;i<n;i++){
-        //     System.out.print(prefix[i] + ", ");
-        // }
-        // System.out.println("");
-        // for(int i=0;i<n;i++){
-        //     System.out.print(postfix[i] + ", ");
-        // }
-        for(int i=0;i<n;i++){
-            if(i==0){
-                output[i] = postfix[i+1];
-            }else if(i==n-1){
-                output[i] = prefix[i-1];
-            }else{
-                output[i] = prefix[i-1] * postfix[i+1];
-            }
+            output[i] = output[i]*postFix;
+            postFix = postFix*nums[i];
         }
         return output;
     }
