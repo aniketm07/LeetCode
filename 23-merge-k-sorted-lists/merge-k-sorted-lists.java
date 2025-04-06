85ff88fd533cba5bad114032cfb6fdb3ec6f6b4c
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-06 15:22:50 -0300

    Time: 4 ms (71.97%) | Memory: 44.8 MB (28.16%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,41 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+class NewListNode {
+    int val;
+    ListNode node;
+}
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0){
+        if (lists.length == 0) {
             return null;
         }
-        if(lists.length==1){
-            return lists[0];
-        }
-        ListNode prevList = lists[0];
-        for(int i=1;i<lists.length;i++){
-            ListNode newList = lists[i];
-            prevList = mergeList(prevList, newList);
-        }
-        return prevList;
-    }
+        ListNode head = new ListNode(-1);
+        ListNode ptr = head;
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((node1, node2) -> node1.val - node2.val);
 
-    public ListNode mergeList(ListNode list1, ListNode list2){
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                temp.next = list1;
-                list1 = list1.next;
-            }else{
-                temp.next = list2;
-                list2 = list2.next;
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.offer(node);
             }
-            temp = temp.next;
         }
-        if(list1!=null){
-            temp.next = list1;
+
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+            ptr.next = node;
+            ptr = ptr.next;
+            if(node.next!=null){
+                minHeap.offer(node.next);
+            }
         }
-        if(list2!=null){
-            temp.next = list2;
-        }
-        return dummy.next;
+
+        return head.next;
     }
 }
