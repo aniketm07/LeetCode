014ae2053d83a946fea09c698f37a719a2f3dfe6
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-07 11:28:37 -0300

    Time: 1 ms (47.27%) | Memory: 44.9 MB (82.98%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int og = image[sr][sc];
-        if(og != color) {
-            dfs(image, sr, sc, og, color);
+        int oldColor = image[sr][sc];
+        if (oldColor == color) return image;
+
+        int n = image.length;
+        int m = image[0].length;
+
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // Up, Down, Left, Right
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sr, sc});
+        image[sr][sc] = color;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int row = curr[0];
+            int col = curr[1];
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m
+                        && image[newRow][newCol] == oldColor) {
+                    image[newRow][newCol] = color;
+                    queue.offer(new int[]{newRow, newCol});
+                }
+            }
         }
+
         return image;
-    }
-
-    private void dfs(int[][] image, int r, int c, int ogColor, int newColor) {
-        if(r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != ogColor) {
-            return;
-        }
-
-        image[r][c] = newColor;
-
-        dfs(image, r+1, c, ogColor, newColor);
-        dfs(image, r-1, c, ogColor, newColor);
-        dfs(image, r, c+1, ogColor, newColor);
-        dfs(image, r, c-1, ogColor, newColor);
     }
 }
