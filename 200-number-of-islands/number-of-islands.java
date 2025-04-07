b35c73f233a5cf0dfb0e3b0ae7db09d38504f182
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-06 22:23:32 -0300

    Time: 2 ms (99.80%) | Memory: 49.7 MB (52.58%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,12 +2,11 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
         int count = 0;
         for(int i=0; i<n; i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && !visited[i][j]){
-                    bfs(grid, visited, i, j, n, m);
+                if(grid[i][j] == '1'){
+                    bfs(grid, i, j, n, m);
                     count++;
                 }
             }
@@ -15,32 +14,16 @@ class Solution {
         return count;
     }
 
-    public void bfs(char[][] grid, boolean[][] visited, int i, int j, int n, int m){
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
-        visited[i][j] = true;
-        // TOP
-        if(i-1>=0 && grid[i-1][j] == '1' && !visited[i-1][j]){
-            visited[i-1][j] = true;
-            queue.offer(new Pair(i-1,j));
+    public void bfs(char[][] grid, int i, int j, int n, int m){
+        if(i<0 || i>= n || j<0 || j>= m){
+            return;
         }
-        // Bottom
-        if(i+1<n && grid[i+1][j] == '1' && !visited[i+1][j]){
-            visited[i+1][j] = true;
-            queue.offer(new Pair(i+1,j));
-        }
-        // RIGHT
-        if(j+1<m && grid[i][j+1] == '1' && !visited[i][j+1]){
-            visited[i][j+1] = true;
-            queue.offer(new Pair(i,j+1));
-        }
-        // LEFT
-        if(j-1>=0 && grid[i][j-1] == '1' && !visited[i][j-1]){
-            visited[i][j-1] = true;
-            queue.offer(new Pair(i,j-1));
-        }
-        while(queue.size() > 0){
-            Pair<Integer, Integer> pair = queue.poll();
-            bfs(grid, visited, pair.getKey(), pair.getValue(), n, m);
+        if(grid[i][j]=='1'){
+            grid[i][j]='X';
+            bfs(grid,i-1,j,n,m);
+            bfs(grid,i+1,j,n,m);
+            bfs(grid,i,j-1,n,m);
+            bfs(grid,i,j+1,n,m);
         }
     }
 
