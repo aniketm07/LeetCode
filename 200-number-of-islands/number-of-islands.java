5e0266774283b4540021fd5a23973db1b41e028b
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-07 15:16:07 -0300

    Time: 3 ms (86.00%) | Memory: 49.4 MB (70.80%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,12 +1,14 @@
 class Solution {
+
+    int[][] directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         int count = 0;
-        for(int i=0; i<n; i++){
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid, i, j, n, m);
+                    dfs(grid, i, j);
                     count++;
                 }
             }
@@ -14,25 +16,14 @@ class Solution {
         return count;
     }
 
-    public void dfs(char[][] grid, int i, int j, int n, int m){
-        if(i<0 || i>= n || j<0 || j>= m){
-            return;
-        }
-        if(grid[i][j]=='1'){
-            grid[i][j]='X';
-            dfs(grid,i-1,j,n,m);
-            dfs(grid,i+1,j,n,m);
-            dfs(grid,i,j-1,n,m);
-            dfs(grid,i,j+1,n,m);
+    public void dfs(char[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length) return;
+
+        if(grid[i][j] == '1'){
+            grid[i][j] = 'X';
+            for(int[] dir : directions){
+                dfs(grid, i + dir[0], j + dir[1]);
+            }
         }
     }
-
-    // public void Print(boolean[][] array){
-    //     for (boolean[] x : array) {
-    //         for (boolean y : x){
-    //             System.out.print(y + " ");
-    //         }
-    //         System.out.println();
-    //     }
-    // }
 }
