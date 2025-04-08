746bfa75ba9e96f91d25ed54e745cc2e40a57552
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-08 01:15:32 -0300

    Time: 5 ms (81.85%) | Memory: 45.4 MB (64.07%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
+        for(int i=0; i<numCourses; i++){
             list.add(new ArrayList<>());
         }
-
         int[] indegree = new int[numCourses];
+        for(int i=0;i<numCourses;i++) indegree[i] = 0;
         
-        for (int[] edge : prerequisites) {
+        for(int[] edge : prerequisites){
             list.get(edge[0]).add(edge[1]);
             indegree[edge[1]]++;
         }
-
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
@@ -21,17 +20,17 @@ class Solution {
 
         List<Integer> topoSort = new ArrayList<>();
 
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
+        while(!queue.isEmpty()){
+            Integer node = queue.poll();
             topoSort.add(node);
-            for (int neighbor : list.get(node)) {
-                indegree[neighbor]--; // ✅ correct here
-                if (indegree[neighbor] == 0) {
-                    queue.offer(neighbor);
+            for(int i : list.get(node)){
+                indegree[i]--;
+                if(indegree[i] == 0){
+                    queue.offer(i);
                 }
             }
         }
 
-        return topoSort.size() == numCourses;
+        return (topoSort.size() == numCourses);
     }
 }
