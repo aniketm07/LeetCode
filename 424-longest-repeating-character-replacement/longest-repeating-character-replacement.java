d710d6ab33d63e44acd23ea8065b161fb6800363
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-08 12:09:44 -0300

    Time: 10 ms (72.36%) | Memory: 44.7 MB (73.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,35 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // Initialising an empty array to store the count of the 
-        // characters in the given string s
-        int[] arr = new int[26];
-        int res = 0;
-        int max = 0;
-
-        // The left pointer for the sliding window is l AND r is the 
-        // right pointer
         int l = 0;
-        for (int r = 0; r < s.length(); r++) {
-            // Counting the number of each character in the string s
-            arr[s.charAt(r) - 'A']++;
+        int max = 0;
+        int[] freq = new int[26];
+        int res = 0;
 
-            // Checking the character with max number of occurrence
-            max = Math.max(max, arr[s.charAt(r) - 'A']);
-
-            // Now we check if our current window is valid or not
-            if (r - l + 1 - max > k) { 
-            // this means the no. of replacements is more than
-            // allowed (k)
-                // Decrementing the count of the character which was 
-                // at l because it is no longer in the window
-                arr[s.charAt(l) - 'A']--;
+        for(int r=0;r<s.length();r++){
+            char right = s.charAt(r);
+            freq[right - 'A']++;
+            max = Math.max(max, freq[right - 'A']);
+            // 
+            if(r-l+1-max > k){
+                freq[s.charAt(l) - 'A']--;
                 l++;
             }
-
-            // The max our window can be
-            res = Math.max(res, r - l + 1);
+            res = Math.max(res, r-l+1);
         }
-
         return res;
     }
 }
