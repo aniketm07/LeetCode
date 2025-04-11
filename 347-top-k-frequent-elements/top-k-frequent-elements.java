b2a1f06d80c4c1dd03d65b982efcff1558b34764
Aniket Mhatre <37346075+aniketm07@users.noreply.github.com>
2025-04-11 11:19:14 -0300

    Time: 14 ms (59.78%) | Memory: 47.5 MB (82.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i : nums){
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-        List<Integer>[] bucketList = new ArrayList[nums.length+1];
         int[] ans = new int[k];
-        int j=0;
-        map.entrySet().parallelStream().forEach(element -> {
-            if (bucketList[element.getValue()]==null)
-                bucketList[element.getValue()] = new ArrayList<>();
-            bucketList[element.getValue()].add(element.getKey());
-        });
-        for(int i=bucketList.length-1; i>=0 && j<k; i--){
-            if(bucketList[i]!=null){
-                for(int num : bucketList[i]){
-                    ans[j] = num;
-                    j++;
-                }
-            }
+        Map<Integer, Integer> hm = new HashMap<>();
+        
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
+        
+        List<int[]> temp = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            temp.add(new int[]{entry.getValue(), entry.getKey()});
+        }
+        
+        Collections.sort(temp, (a, b) -> b[0] - a[0]);
+        
+        for (int i = 0; i < k; i++) {
+            ans[i] = temp.get(i)[1];
+        }
+        
         return ans;
     }
 }
